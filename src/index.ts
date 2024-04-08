45ea94197cdab36b6a5dc904feaf9bf5a38bc281
Akash Indulkar <134040770+akash-indulkar@users.noreply.github.com>
2024-04-08 21:26:47 +0530

    Update index.ts
    enabled env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
+import express from "express";
 import cors from "cors";
 import { Pool } from 'pg';
 import v1Router from "./routes/index"
